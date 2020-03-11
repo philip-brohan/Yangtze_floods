3e3cafb39dbd1fe41814af57cfe706c5c6ff2c4a
Philip Brohan <philip@brohan.org>
2020-03-11 15:46:25 +0000

    Added Temperature time-series scripts

--- get_data/get_data_V3.py
+++ get_data/get_data_V3.py
@@ -11,6 +11,8 @@ for year in range(1926,1936):
     twcr.fetch('PRATE',datetime.datetime(year,1,1),version='3')
     twcr.fetch('TMP2m',datetime.datetime(year,1,1),version='3')
 
-# Winds only for 1931
+# Winds only for 1930 and 1931
 twcr.fetch('UGRD10m',datetime.datetime(1931,1,1),version='3')
 twcr.fetch('VGRD10m',datetime.datetime(1931,1,1),version='3')
+twcr.fetch('UGRD10m',datetime.datetime(1930,1,1),version='3')
+twcr.fetch('VGRD10m',datetime.datetime(1930,1,1),version='3')
